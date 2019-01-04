c378a793ef61a2c9ce0b7b27007062fd7c9571dc
Antony Acosta <antonyacosta78@gmail.com>
2019-01-04 14:57:18 -0300

    a

--- Collector.class.php
+++ Collector.class.php
@@ -2,37 +2,49 @@
 
 class Collector{
 
-    private $handlers;
+    public $handlers;
 
     public function get(string $route, $function){
-        if(is_callable($function)){
-            $this->handlers[$route] = [
-                "type"=>"get",
-                "call"=>$function
-            ];
-        }
+        $this->addRoute($route, "get", $function);
     }
 
     public function post(string $route, $function){
-        if(is_callable($function)){
-            $this->handlers[$route] = [
-                "type"=>"get",
-                "call"=>$function
-            ];
-        }
+        $this->addRoute($route, "post", $function);
     }
 
     public function any(string $route, $function){
-        if(is_callable($function)){
-            $this->handlers[$route] = [
-                "type"=>"any",
-                "call"=>$function
-            ];
-        }
+       $this->addRoute($route, "any", $function);
     }
 
-    public function call($route){
-        $this->handlers[$route]["call"]();
+    public function checkMethod(int $index, string $method){
+        return strtolower($method) === $this->handlers[$index]["method"];
     }
+
+    public function call(int $index, array $args = []){
+        if($args === []){
+            $this->handlers[$index]["call"]();
+        }else{
+            $this->handlers[$index]["call"](...$args);
+        }
+        
+    }
+
+    private function addRoute($route, $method, $closure){
+        if(is_callable($closure)){
+            $this->handlers[] = [
+                "method"=>$method,
+                "call"=>$closure
+            ];
+            return true;
+        }
+        return false;
+    }
+
+    private function parseRoute($route){
+        //returns params as indexed Array(in order)
+        $route = rtrim($route, "/");
+        if($route === ""){
+            return [];
+        }
 
 }
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php 
-echo "Hello World!".PHP_EOL;
+// echo "Hello World!".PHP_EOL;
 
-include_once "Collector.class.php";
-include_once "Router.class.php";
+// include_once "Collector.class.php";
+// include_once "Router.class.php";
 
-$collector = new Collector;
+// $collector = new Collector;
 
-$collector->get("/",function(){
-    echo "It's Working";
-});
+// $collector->get("/",function(){
+//     echo "It's Working";
+// });
 
-$router = new Router($collector);
-$router->resolve("/");
+// $router = new Router($collector);
+// $router->resolve("/");
+var_dump(rtrim("/","/"));
