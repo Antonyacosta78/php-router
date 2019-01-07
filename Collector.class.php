11c25f1e2699c1ad7822011a08338955457c3c14
Antony Acosta <mzr@jfrs.jus.br>
2019-01-07 17:10:24 -0200

    implemented Collector::filters()

--- Collector.class.php
+++ Collector.class.php
@@ -4,6 +4,7 @@ class Collector{
 
     public $handlers;
     public $errorHandlers; 
+    public $filters;
 
     public function error(int $error, $closure){
         if(is_callable($closure)){
@@ -13,16 +14,24 @@ class Collector{
         return false;
     }
 
-    public function get(string $route, $function){
-        $this->addRoute($route, "get", $function);
+    public function filter($name, $closure){
+        if(is_callable($closure)){
+            $this->filters[$name] = $closure;
+            return true;
+        }
+        return false;
     }
 
-    public function post(string $route, $function){
-        $this->addRoute($route, "post", $function);
+    public function get(string $route, $function, $filters = ["before"=>null, "after"=>null]){
+        $this->addRoute($route, "get", $function, $filters);
     }
 
-    public function any(string $route, $function){
-       $this->addRoute($route, "any", $function);
+    public function post(string $route, $function, $filters = ["before"=>null, "after"=>null]){
+        $this->addRoute($route, "post", $function, $filters);
+    }
+
+    public function any(string $route, $function, $filters = ["before"=>null, "after"=>null]){
+       $this->addRoute($route, "any", $function, $filters);
     }
 
     public function checkMethod(int $index, string $method){
@@ -37,20 +46,29 @@ class Collector{
     }
 
     public function call(int $index, array $args = []){
-        if($args === []){
-            $this->handlers[$index]["call"]();
-        }else{
-            $this->handlers[$index]["call"](...$args);
+        if($this->handlers[$index]["before"] !== null && is_callable($this->filters[$this->handlers[$index]["before"]])){
+            $return = $this->filters[$this->handlers[$index]["before"]]();
         }
-        
+        if(is_null($return)){   
+            if($args === []){
+                $this->handlers[$index]["call"]();
+            }else{
+                $this->handlers[$index]["call"](...$args);
+            }
+            if($this->handlers[$index]["after"] !== null && is_callable($this->filters[$this->handlers[$index]["after"]])){
+                $this->filters[$this->handlers[$index]["after"]]();
+            }    
+        }
     }
 
-    private function addRoute($route, $method, $closure){
+    private function addRoute($route, $method, $closure, $filters){
         if(is_callable($closure)){
             $this->handlers[] = [
                 "method"=>$method,
                 "call"=>$closure,
-                "match"=>$this->parseRoute($route)
+                "match"=>$this->parseRoute($route),
+                "before"=>$filters["before"],
+                "after"=>$filters["after"]
             ];
             return true;
         }
--- index.php
+++ index.php
@@ -14,7 +14,15 @@ $collector->error(404, function(){
 });
 
 $collector->error(405, function(){
-    echo "hey, you should not be using {$_SERVER['REQUEST_METHOD']}";
+    echo "hey, you should not be using ".$_SERVER["REQUEST_METHOD"];
+});
+
+$collector->filter("testbefore", function(){
+    echo "I'm executed before the route. ";
+});
+
+$collector->filter("testafter", function(){
+    echo "I'm executed after the example route. ";
 });
 
 $collector->get("/",function(){
@@ -22,62 +30,13 @@ $collector->get("/",function(){
 });
 
 $collector->any("/example", function(){
-    echo "any is working!";
-});
+    echo " I am the example Route. ";
+}, ['before'=>"testbefore", 'after'=>"testafter"]);
 
 $collector->get("/realroute/{id}/{sec}", function($id, $sec){
-    echo "Params are working! got $id and $sec";
+    echo "Params are working! got $id and $sec . ";
 });
 
 $router = new Router($collector, "/router");
 
-$router->resolve("/router/realroute/2/1");
-
-
-
-// $route = "/nothing/else/matters/1";
-// $example = "/nothing/else/matters/1";
-// $prefix = "/else";
-// echo "<pre>";
-// var_dump($_SERVER);
-// echo "</pre>";
-
-// function resolve($route, $prefix = ""){
-//     $prefix = strlen($prefix);
-//     if($prefix !== 0){
-//         $route = substr_replace($route, "", 0, $prefix);
-//     }
-//     return $route;
-
-// }
-
-// function formatRoute($route){
-//     preg_match_all("/(\{.*?\})/",$route,$keys);
-//     preg_match_all("/\{\w+:?(.*?)\}/",$route,$values);
-//     $pattern = str_replace(
-//                 $keys[1], 
-//                 array_map(function($e){
-//                     return ($e === "") ? "(.*)" : "(".trim($e).")";
-//                 },$values[1]),
-//                 $route
-//             );
-//     return "/^".preg_replace("/\//","\/",$pattern)."$/";
-// }
-
-// echo "<br><br>route<br>";
-// var_dump($route);
-
-
-// echo "<br><br>formatRoute<br>";
-// $formatted = formatRoute($route);
-// var_dump($formatted);
-
-// $match = preg_match($formatted,$example,$matches);
-// //array_shift($matches);
-
-// echo "<br><br>match example<br>";
-
-// var_dump($matches);
-// echo "<br><br>resolve<br>";
-
-// var_dump(resolve($route, $prefix));
+$router->resolve("/router/example");
